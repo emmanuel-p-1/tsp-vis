72105f6cfa3e7df234b7722fc4b473cccd467a41
emmanuel-p-1 <emmanuel.pinca098@gmail.com>
2020-12-02 21:24:48 +1100

    added node creation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
 import './App.css';
+
 import Visualiser from './Visualiser/Visualiser';
 
 function App() {
   return (
     <div className="App">
-      <Visualiser/>
+      <Visualiser />
     </div>
   );
 }
--- src/Visualiser/Visualiser.jsx
+++ src/Visualiser/Visualiser.jsx
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
-import Node from './Node/Node';
-import Edge from './Edge/Edge';
+import { Graph } from 'react-d3-graph';
 
 import './Visualiser.css';
 
 export default class Visualiser extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {
+        nodes: [
+          { id: 0 }
+        ],
+        links: [
+        ]
+      },
+      config: {},
+      nodeCount: 1
+    };
+  }
 
-	render() {
-		return (
-			<div></div>
-		);
-	}
+  createNode = () => {
+    const newNode = `${this.state.nodeCount}`;
+    this.state.data.nodes.push({ id: newNode });
+    this.setState({ data: this.state.data, nodeCount: this.state.nodeCount + 1 });
+  }
+
+  render() {
+    return (
+      <Graph 
+        id="graph"
+        data={this.state.data}
+        config={this.state.config}
+        onClickGraph={this.createNode}
+      />
+    );
+  }
 }
